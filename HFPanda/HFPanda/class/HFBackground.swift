ac5d5206ee335baafdbd4e95609da1cf3c139fde
liqun <liqun@huofar.com>
2018-03-07 23:07:54 +0800

    绘制近处背景纹理和精灵

--- HFPanda/HFPanda/class/HFBackground.swift
+++ HFPanda/HFPanda/class/HFBackground.swift
@@ -24,31 +24,56 @@ class HFBackground:SKNode {
     override init() {
         super.init()
         //远处树木纹理
-        let farSprite = SKTexture.init(imageNamed: "background_f1")
+        let farTexture = SKTexture.init(imageNamed: "background_f1")
         
         //定义远处树木背景精灵
-        let farBg0 = SKSpriteNode.init(texture: farSprite)
-        let farBg1 = SKSpriteNode.init(texture: farSprite)
-        let farBg2 = SKSpriteNode.init(texture: farSprite)
+        let farBg0 = SKSpriteNode.init(texture: farTexture)
+        let farBg1 = SKSpriteNode.init(texture: farTexture)
+        let farBg2 = SKSpriteNode.init(texture: farTexture)
         
         farBg0.position.y =  -100
-        //farBg0.zPosition = 9
+        farBg0.zPosition = 9
         //farBg0.anchorPoint = CGPoint.init(x: 0, y: 0)
         farBg0.position.x =  -UIScreen.main.bounds.size.height/2
 
         farBg1.position.y = -100
-        //farBg1.zPosition = 9
+        farBg1.zPosition = 9
         //farBg1.anchorPoint = CGPoint.init(x: 0, y: 0)
         farBg1.position.x = farBg0.position.x + farBg0.frame.width
         
         farBg2.position.y = -100
-        //farBg2.zPosition = 9
+        farBg2.zPosition = 9
         farBg2.anchorPoint = CGPoint.init(x: 0, y: 0)
         farBg2.position.x = farBg1.position.x + farBg0.frame.width
         
         self.addChild(farBg0)
         self.addChild(farBg1)
         self.addChild(farBg2)
+        //构建集合
+        arrFar.append(farBg0)
+        arrFar.append(farBg1)
+        arrFar.append(farBg2)
         
+        //近处背景纹理
+        let nearTure = SKTexture.init(imageNamed:"background_f0")
+        //近处背景精灵
+        let nearBg0 = SKSpriteNode.init(texture: nearTure)
+        nearBg0.zPosition = 10
+        //nearBg0.anchorPoint = CGPoint.init(x: 0, y: 0)
+        nearBg0.position.y = -130
+        nearBg0.position.x = -UIScreen.main.bounds.size.height/2
+        
+        let nearBg1 = SKSpriteNode.init(texture: nearTure)
+        nearBg1.zPosition = 10
+        //nearBg1.anchorPoint = CGPoint.init(x: 0, y: 0)
+        nearBg1.position.y = -130
+        nearBg1.position.x = nearBg0.position.x + nearBg0.frame.width
+    
+        
+        self.addChild(nearBg0)
+        self.addChild(nearBg1)
+        //构建集合
+        arrBG.append(nearBg0)
+        arrBG.append(nearBg1)
     }
 }
